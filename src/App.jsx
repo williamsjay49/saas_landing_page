e1ef3d95cc392f2da70609b30ccf7f4d4f541399
johnson williams <williams.gb55@gmail.com>
2025-04-02 01:26:01 +0900

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
+};
+
+export default App;
